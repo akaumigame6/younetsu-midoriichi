391a409f141b5bc367177ed2569a7d78c3eee350
Akaumigame <106215296+akaumigame6@users.noreply.github.com>
2026-06-30 14:15:26 +0900

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/YouNetsu/', // 本番デプロイ時のベースパス
+  base: '/younetsu-midoriichi/', // 本番デプロイ時のベースパス
 })
